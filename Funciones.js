b7997a9dcd17b7a3685d9abe0e2378f9fc8bebae
JuanDGT <jdgt@unicauca.edu.co>
2023-03-13 08:25:30 -0500

    Agrego presentacion y HM

---
+++ Funciones.js
@@ -0,0 +1,39 @@
+/*
+        1. Crear una funcion que reciba 2 parámetros numéricos y devuelva la suma, 
+        la funcion llámela suma.
+*/
+
+// Escriba la función debajo:
+
+
+/* 
+        2. Crear una función que imprima en pantalla un mensaje de bienvenida.
+*/
+
+// Escriba la función debajo:
+
+/* 
+        3. Crear una función la cual se le pase un número y que retorne verdadero si es par,
+         en caso contrario, devuelva falso.
+*/
+
+// Escriba la función debajo:
+
+/* 
+        4. Crear una función que permita saber si un numero es primo.
+*/
+
+// Escriba la función debajo:
+
+/* 
+        5. Crear una función que devuelva el area de un circulo, ingresando el radio.
+*/
+
+// Escriba la función debajo:
+
+/* 
+        6. Crear una función que pida un número y devuelva cuántos múltiplos de 3 hay, desde 1 hasta ese número.
+*/
+
+// Escriba la función debajo:
+
